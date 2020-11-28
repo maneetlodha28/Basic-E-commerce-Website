fc54c580932de05591e438e21e1597ae2de84bd6
Maneet Lodha <36129353+maneetlodha28@users.noreply.github.com>
2020-11-29 01:20:12 +0530

    contact update

--- src/core/Base.js
+++ src/core/Base.js
@@ -19,7 +19,13 @@ const Base = ({
     <footer className="footer bg-dark mt-auto py-2">
       <div className="conatiner-fluid bg-danger text-white text-center py-3">
         <h4>If you got any questions feel free to reach out</h4>
-        <button className="btn btn-warning btn-sm">Contact Us</button>
+        <a
+          href="mailto:maneetlodha28@gmail.com"
+          className="fa fa-envelope"
+          className="btn"
+        >
+          Contact us{" "}
+        </a>
       </div>
       <div className="container">
         <span className="text-muted">
