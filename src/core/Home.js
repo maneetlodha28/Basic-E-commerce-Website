ddec86e33b1f38dda879caec43f3caf388a16bb2
Maneet Lodha <36129353+maneetlodha28@users.noreply.github.com>
2020-12-22 01:06:31 +0530

    ui: add loading text for products

--- src/core/Home.js
+++ src/core/Home.js
@@ -8,15 +8,29 @@ import { getProducts } from "./helper/coreapicalls";
 export default function Home() {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(false);
-
+  const [loading, setLoading] = useState(false);
   const loadAllProduct = () => {
+    setLoading(true);
     getProducts().then((data) => {
       if (data.error) {
         setError(data.error);
       } else {
+        setLoading(false);
         setProducts(data);
       }
     });
+  };
+  const loadingMessage = () => {
+    return (
+      loading && (
+        <div
+          className="alert alert-info "
+          style={{ textAlign: "center", marginLeft: 5 + "rem" }}
+        >
+          <h3>Loading...</h3>
+        </div>
+      )
+    );
   };
 
   useEffect(() => {
@@ -29,6 +43,7 @@ export default function Home() {
         <h2 style={{ color: "gray", fontStyle: "italic", textAlign: "start" }}>
           All T-Shirts
         </h2>
+        {loadingMessage()}
         <div className="row justify-content-center">
           {products.map((product, index) => {
             return (
